1e8ada405a2cdfe892f45d393e293952ab1379df
Xenchu <72039294+Xenchu@users.noreply.github.com>
2022-03-02 11:29:48 -0500

    Add project files.

---
+++ FinAEC/CountDownAEC.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinAEC
+{
+    public class CountDownAEC
+    {
+        public DateTime DateFin { get; set; }
+
+        public CountDownAEC(int yearEnd, int monthEnd, int dayEnd)
+        {
+            DateFin = new DateTime(yearEnd, monthEnd, dayEnd);
+        }
+
+        public Tuple<int, int, int, TimeSpan> GetTimeLeft()
+        {
+            var now = DateTime.Now;
+
+            var year = DateFin.Year - now.Year;
+            var yearLeft = year < 0 ? 0 : year;
+
+            var month = DateFin.Month - now.Month;
+            var monthLeft = month < 0 ? 0 : month;
+
+            var days = DateFin.Day - now.Day;
+            var daysLeft = days < 0 ? 0 : days;
+
+            var timeleft = DateFin - now;
+
+            return new Tuple<int, int, int, TimeSpan>(yearLeft, monthLeft, daysLeft, timeleft);
+        }
+    }
+}
---
+++ FinAEC/Program.cs
@@ -0,0 +1,45 @@
+﻿using FinAEC;
+
+const int year = 2022;
+const int month = 7;
+const int Day = 13;
+
+var countDownAEC = new CountDownAEC(year, month, Day);
+
+do
+{
+    Console.Clear();
+    var timeLeft = countDownAEC.GetTimeLeft();
+    var text = string.Empty;
+
+    Console.WriteLine();
+    var horizontalBar = "|";
+    var verticalBar = "-------------------------------------------------------------------";
+    var header = String.Format("{0,1} {1,7} {2,1} {3,8} {4,1} {5,6} {6,1} {7,7} {8,1} {9,10} {10,1} {11,10} {12,1}",
+                            horizontalBar, "Year(s)",
+                            horizontalBar, "Month(s)",
+                            horizontalBar, "Day(s)",
+                            horizontalBar, "Hour(s)",
+                            horizontalBar, "Minutes(s)",
+                            horizontalBar, "Seconds(s)",
+                            horizontalBar);
+
+    var body = String.Format("{0,1} {1,7} {2,1} {3,8} {4,1} {5,6} {6,1} {7,7} {8,1} {9,10} {10,1} {11,10} {12,1}",
+                            horizontalBar, timeLeft.Item1,
+                            horizontalBar, timeLeft.Item2,
+                            horizontalBar, timeLeft.Item3,
+                            horizontalBar, timeLeft.Item4.Hours,
+                            horizontalBar, timeLeft.Item4.Minutes,
+                            horizontalBar, timeLeft.Item4.Seconds,
+                            horizontalBar);
+
+
+    Console.WriteLine(verticalBar);
+    Console.WriteLine(header);
+    Console.WriteLine(verticalBar);
+    Console.WriteLine(body);
+    Console.WriteLine(verticalBar);
+
+    System.Threading.Thread.Sleep(1000);
+} while (true);
+
